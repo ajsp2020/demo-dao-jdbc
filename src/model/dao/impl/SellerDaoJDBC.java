7dd8f49c2566fce6fa3ce782b77ef126ae633d65
Joao Pereira <ajpereira88@gmail.com>
2021-11-05 15:02:04 -0300

    findAll implemetation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -87,9 +87,47 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName \r\n"
+					+ "FROM seller INNER JOIN department \r\n" 
+					+ "ON seller.DepartmentId = department.Id\r\n"
+					+ "ORDER BY Name");
+		
+			rs = st.executeQuery();
+
+			List<Seller> list = new ArrayList<>();
+			// Para evitar criar um departamento diferente e pontar o vendedor sempre para o
+			// mesmo departamento
+			Map<Integer, Department> map = new HashMap<>();
+			while (rs.next()) {
+
+				// Verificando se o departamento já existe:
+				Department dep = map.get(rs.getInt("DepartmentId"));
+
+				if (dep == null) { // Se não tiver o departamento
+					dep = instantiateDepartment(rs); // Instancia o departamento
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+			
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+
 	}
+	
 
 	@Override
 	public List<Seller> findByDepartment(Department department) {
